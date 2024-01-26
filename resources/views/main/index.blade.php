1012d1839b694f77f0378a98d4b07208d699a79d
Satyanr <satyanr84@gmail.com>
2024-01-26 15:09:57 +0700

    some view

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -12,7 +12,8 @@
                         <div class="row justify-content-start">
                             <div class="col-sm-10 col-lg-8">
                                 <h1 class="display-3 text-white animated slideInDown">AWAN PENGGERAK</h1>
-                                <p class="fs-5 text-white mb-4 pb-2">Solusi Peningkatan Kompetensi Pendidik dan Tenaga Kependidikan di Daerah Khusus/3T</p>
+                                <p class="fs-5 text-white mb-4 pb-2">Solusi Peningkatan Kompetensi Pendidik dan Tenaga
+                                    Kependidikan di Daerah Khusus/3T</p>
                             </div>
                         </div>
                     </div>
@@ -26,7 +27,8 @@
                         <div class="row justify-content-start">
                             <div class="col-sm-10 col-lg-8">
                                 <h3 class="text-white animated slideInDown">
-                                    Ekosistem Awan Penggerak memudahkan pendidik dan tenaga kependidikan untuk mengakses informasi, referensi, serta inspirasi yang ada dalam Platform Merdeka Mengajar (PMM)
+                                    Ekosistem Awan Penggerak memudahkan pendidik dan tenaga kependidikan untuk mengakses
+                                    informasi, referensi, serta inspirasi yang ada dalam Platform Merdeka Mengajar (PMM)
                                 </h3>
                                 <p class="fs-5 text-white mb-4 pb-2">Untuk mengakses beragam informasi tersebut,
                                     silahkan kunjungi fitur-fitur unggulan Awan Penggerak</p>
@@ -201,125 +203,7 @@
 
 
     <!-- Courses Start -->
-    <div class="container-xxl py-5">
-        <div class="container">
-            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h6 class="section-title bg-white text-center text-primary px-3">Courses</h6>
-                <h1 class="mb-5">Popular Courses</h1>
-            </div>
-            <div class="row g-4 justify-content-center">
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <a href="#">
-                        <div class="course-item bg-light h-100 d-flex flex-column">
-                            <div class="position-relative overflow-hidden">
-                                <img class="img-fluid image-fixed-size-cus" src="/mainasset/img/course-1.jpg"
-                                    alt="">
-                                {{-- <div class="w-100 d-flex justify-content-center position-absolute bottom-0 start-0 mb-4">
-                                    <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3 border-end"
-                                        style="border-radius: 30px 0 0 30px;">Read More</a>
-                                    <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3"
-                                        style="border-radius: 0 30px 30px 0;">Join Now</a>
-                                </div> --}}
-                            </div>
-                            <div class="text-center p-4 pb-0">
-                                {{-- <h3 class="mb-0">$149.00</h3>
-                                <div class="mb-3">
-                                    <small class="fa fa-star text-primary"></small>
-                                    <small class="fa fa-star text-primary"></small>
-                                    <small class="fa fa-star text-primary"></small>
-                                    <small class="fa fa-star text-primary"></small>
-                                    <small class="fa fa-star text-primary"></small>
-                                    <small>(123)</small>
-                                </div> --}}
-                                <h5 class="mb-4">Web Design & Development Course for Beginners</h5>
-                            </div>
-                            <div class="d-flex border-top text-dark mt-auto">
-                                <small class="flex-fill text-center border-end py-2"><i
-                                        class="fa fa-user-tie text-primary me-2"></i>John Doe</small>
-                                <small class="flex-fill text-center py-2"><i
-                                        class="fa fa-clock text-primary me-2"></i>1.49
-                                    Hrs</small>
-                                {{-- <small class="flex-fill text-center py-2"><i class="fa fa-user text-primary me-2"></i>30
-                                    Students</small> --}}
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                    <a href="#">
-                        <div class="course-item bg-light h-100 d-flex flex-column">
-                            <div class="position-relative overflow-hidden">
-                                <img class="img-fluid image-fixed-size-cus" src="/mainasset/img/course-2.jpg"
-                                    alt="">
-                                {{-- <div class="w-100 d-flex justify-content-center position-absolute bottom-0 start-0 mb-4">
-                                    <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3 border-end"
-                                        style="border-radius: 30px 0 0 30px;">Read More</a>
-                                    <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3"
-                                        style="border-radius: 0 30px 30px 0;">Join Now</a>
-                                </div> --}}
-                            </div>
-                            <div class="text-center p-4 pb-0">
-                                {{-- <h3 class="mb-0">$149.00</h3>
-                                <div class="mb-3">
-                                    <small class="fa fa-star text-primary"></small>
-                                    <small class="fa fa-star text-primary"></small>
-                                    <small class="fa fa-star text-primary"></small>
-                                    <small class="fa fa-star text-primary"></small>
-                                    <small class="fa fa-star text-primary"></small>
-                                    <small>(123)</small>
-                                </div> --}}
-                                <h5 class="mb-4">Web Design & Development Course for Beginners</h5>
-                            </div>
-                            <div class="d-flex border-top text-dark mt-auto">
-                                <small class="flex-fill text-center border-end py-2"><i
-                                        class="fa fa-user-tie text-primary me-2"></i>John Doe</small>
-                                <small class="flex-fill text-center py-2"><i
-                                        class="fa fa-clock text-primary me-2"></i>1.49 Hrs</small>
-                                {{-- <small class="flex-fill text-center py-2"><i class="fa fa-user text-primary me-2"></i>30
-                                    Students</small> --}}
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                    <a href="#">
-                        <div class="course-item bg-light h-100 d-flex flex-column">
-                            <div class="position-relative overflow-hidden">
-                                <img class="img-fluid image-fixed-size-cus" src="/mainasset/img/course-3.jpg"
-                                    alt="">
-                                {{-- <div class="w-100 d-flex justify-content-center position-absolute bottom-0 start-0 mb-4">
-                                    <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3 border-end"
-                                        style="border-radius: 30px 0 0 30px;">Read More</a>
-                                    <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3"
-                                        style="border-radius: 0 30px 30px 0;">Join Now</a>
-                                </div> --}}
-                            </div>
-                            <div class="text-center p-4 pb-0">
-                                {{-- <h3 class="mb-0">$149.00</h3>
-                                <div class="mb-3">
-                                    <small class="fa fa-star text-primary"></small>
-                                    <small class="fa fa-star text-primary"></small>
-                                    <small class="fa fa-star text-primary"></small>
-                                    <small class="fa fa-star text-primary"></small>
-                                    <small class="fa fa-star text-primary"></small>
-                                    <small>(123)</small>
-                                </div> --}}
-                                <h5 class="mb-4">Web Design & Development Course for Beginners</h5>
-                            </div>
-                            <div class="d-flex border-top text-dark mt-auto">
-                                <small class="flex-fill text-center border-end py-2"><i
-                                        class="fa fa-user-tie text-primary me-2"></i>John Doe</small>
-                                <small class="flex-fill text-center py-2"><i
-                                        class="fa fa-clock text-primary me-2"></i>1.49 Hrs</small>
-                                {{-- <small class="flex-fill text-center py-2"><i class="fa fa-user text-primary me-2"></i>30
-                                    Students</small> --}}
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
+    @livewire('main.diklat.index')
     <!-- Courses End -->
 
 
